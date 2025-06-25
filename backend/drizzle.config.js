35c3158c1b1e25b73e6cd3b4221a5661e66b839c
Vaishali Tripathi <100984846+vaishalihere@users.noreply.github.com>
2025-06-25 08:50:53 +0530

    Delete backend directory

--- backend/drizzle.config.js
+++
@@ -1,8 +0,0 @@
-import { ENV } from "./src/config/env.js";
-
-export default {
-  schema: "./src/db/schema.js",
-  out: "./src/db/migrations",
-  dialect: "postgresql",
-  dbCredentials: { url: ENV.DATABASE_URL },
-};
